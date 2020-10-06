3c8efc52b6a4cd2dd331248a49356139f7e0aba7
mszulics <mszulics@gmail.com>
2020-10-06 02:05:27 +0200

    Upload chapter 4 drills

--- source/Drills/drill4.cpp
+++ source/Drills/drill4.cpp
@@ -1,47 +1,44 @@
 #include "std_lib_facilities.h"
 
 int main(){
-	double num1;
+	double num;
 	string unit;
-	double maxValue = 0;
-	double minValue = 0;
-	double numTemp;
-	string unitTemp;
+	double maxValue;
+	double minValue;
 	double sum = 0;
+	string unitm = "m";
 	int loopCounter = 0;
 	vector<string> values;
-	while( cin >> num1 >> unit){
-		numTemp = num1;
-		unitTemp = unit;
-		if(unitTemp == "ft"){
-			numTemp * 12;
-			unitTemp = "in";
+	while( cin >> num >> unit){
+		values.push_back(to_string(num)+unit);
+		if(unit == "ft"){
+			num = num * 0.3048;
+			unit = unitm;
 		}
-		if(unitTemp == "in"){
-			numTemp * 2.54;
-			unitTemp = "cm";
+		else if(unit == "in"){
+			num = num * 0.0254;
+			unit = unitm;
 		}
-		if(unitTemp == "cm"){
-			numTemp * 100;
-			unitTemp = "m";
+		else if(unit == "cm"){
+			num = num * 0.01;
+			unit = unitm;
 		}
-		if(unitTemp == "m"){
-			sum += numTemp;
-			values.push_back(to_string(num1)+unit);
+		if(unit == unitm){
+			sum += num;
 			if(loopCounter == 0){
-				minValue = numTemp;
-				maxValue = numTemp;
+				minValue = num;
+				maxValue = num;
 				++loopCounter;
 				cout << "The largest and the smallest number as well so far" << endl;
 			}
 			if(loopCounter != 0){
-				if(numTemp > maxValue){
+				if(num > maxValue){
 					cout << "The largest so far\n";
-					maxValue = numTemp;
+					maxValue = num;
 				}
-				else if(numTemp < minValue){
+				else if(num < minValue){
 					cout << "The smallest so far\n";
-					minValue = numTemp;
+					minValue = num;
 				}
 				++loopCounter;
 			}
@@ -51,7 +48,7 @@ int main(){
 		}
 	}
 	cout << "The given values:\n";
-	for (auto value:values){
+	for (string value:values){
 		cout << value << endl;
 	}
 	cout << "The sum of the values in meter: " << sum << endl;
